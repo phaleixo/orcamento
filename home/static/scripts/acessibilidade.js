c0ba8e25220a30d2a500ea4994b276be1f09afa0
phaleixo <phaleixo1@gmail.com>
2024-09-28 19:09:30 -0300

    new file:   home/static/scripts/acessibilidade.js

---
+++ home/static/scripts/acessibilidade.js
@@ -0,0 +1,21 @@
+/* JavaScript para aumentar e diminuir o tamanho da fonte*/
+const increaseFontBtn = document.getElementById('increaseFont');
+const decreaseFontBtn = document.getElementById('decreaseFont');
+const body = document.querySelector('body');
+
+let fontSize = 16; // Tamanho inicial da fonte em pixels
+
+increaseFontBtn.addEventListener('click', () => {
+    if (fontSize < 22) { // Limite máximo de 22px
+        fontSize += 2;
+        body.style.fontSize = fontSize + 'px';
+    }
+});
+
+decreaseFontBtn.addEventListener('click', () => {
+    if (fontSize > 16) { // Limite mínimo de 16px
+        fontSize -= 2;
+        body.style.fontSize = fontSize + 'px';
+    }
+});
+
